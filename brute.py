54f46115c481569a85119f3593197d66d0ca5457
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-08-15 14:35:51 +0100

    Update brute.py

--- brute.py
+++ brute.py
@@ -42,7 +42,7 @@ print(" TARGET MAIL /PROF LINK:",email+"")
 
 i=0
 while file:
-    file=open('password.txt','o','r' )
+    file=open('password.txt','o','r')
     file.readline()
     i=+1
     if len(file) < 11:
